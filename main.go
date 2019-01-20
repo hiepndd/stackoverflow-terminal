b15d3c7de5ef019ffafca128de3631e414dc7072
hiepndd <nguyendangdaihiep@gmail.com>
2019-01-21 00:26:00 +0700

    implement search query

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var questions_regex = regexp.MustCompile(`questions/\d+/`)
+
+func isQuestion(link string) bool {
+	return questions_regex.Find([]byte(link)) != nil
+}
+
+func performSearch(query string) ([]string, error) {
+	searchURL := "http://www.google.com/search?q=site:stackoverflow.com/questions%20" + url.QueryEscape(query)
+	resp, err := http.Get(searchURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(io.Reader(resp.Body))
+	if err != nil {
+		log.Fatal("error reading document", err)
+	}
+	var links []string
+	doc.Find("h3.r a").Each(func(i int, s *goquery.Selection) {
+		str, exists := s.Attr("href")
+		if exists {
+			u, err := url.Parse(str)
+			if err != nil {
+				log.Fatal(err)
+			}
+			m, _ := url.ParseQuery(u.RawQuery)
+			link := m["q"][0]
+			if isQuestion(link) {
+				links = append(links, link)
+			}
+
+		}
+	})
+
+	if len(links) == 0 {
+		return nil, errors.New("search failed")
+	}
+
+	return links, nil
+}
+
+func main() {
+	query := strings.Join(os.Args[1:], " ")
+	fmt.Println(query)
+}
