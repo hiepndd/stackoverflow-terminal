21d17f99c33721ca23ba8038d8ecf3ec9af4a697
hiepndd <nguyendangdaihiep@gmail.com>
2019-01-28 16:36:48 +0700

    implement search question

--- main.go
+++ main.go
@@ -20,6 +20,26 @@ func isQuestion(link string) bool {
 	return questions_regex.Find([]byte(link)) != nil
 }
 
+func parseEmbededReferenceLinks(doc *goquery.Document) {
+	doc.Find(".answercell .post-text").First().Find("a").Each(func(i int, s *goquery.Selection) {
+		href, success := s.Attr("href")
+		if success == true {
+			href = " (" + href + ")"
+			s.AppendHtml(href)
+		}
+	})
+}
+
+func performRequest(url string) string {
+	doc, err := goquery.NewDocument(url + "?answertab=votes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	parseEmbededReferenceLinks(doc)
+	return doc.Find(".answercell .post-text").First().Text()
+
+}
+
 func performSearch(query string) ([]string, error) {
 	searchURL := "http://www.google.com/search?q=site:stackoverflow.com/questions%20" + url.QueryEscape(query)
 	resp, err := http.Get(searchURL)
@@ -57,7 +77,19 @@ func performSearch(query string) ([]string, error) {
 	return links, nil
 }
 
+func printAnswer(answer, url string) {
+	fmt.Println(answer)
+	fmt.Println("Url:", url)
+
+}
+
 func main() {
 	query := strings.Join(os.Args[1:], " ")
-	fmt.Println(query)
+	questions, err := performSearch(query)
+	if err != nil {
+		log.Fatal("sorry, i couldn't find what you're looking for :(")
+	}
+	fmt.Println("Hello World")
+	answer := performRequest(questions[0])
+	printAnswer(answer, questions[0])
 }
